ab357b8ae10b6b667ea2dd26d0e61d1e7abf5158
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-24 12:25:26 -0500

    Wrote query & passed into component

--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+import { LaunchTile, Header, Button, Loading } from '../components'
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`
 
 export default function Launches() {
-  return <div />;
+  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  if (loading) return <Loading />;
+  if (error) return <p>Error</p>;
+
+  return (
+    <>
+      <Header />
+      {data.launches.launches &&
+        data.launches.launches.map(launch => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+    </>
+  )
 }
