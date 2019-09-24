0b31a234ff95b365046d680b85a730dd0c852ce2
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-24 14:10:28 -0500

    Querying local data with Apollo...

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,20 +1,44 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'
+import { resolvers, typeDefs } from './resolvers'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Pages from './pages'
+import Login from './pages/login'
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/'
-})
 
 const client = new ApolloClient({
   cache,
-  link
+  link: new HttpLink({
+    uri: 'http://localhost:4000/',
+    headers: {
+      authorization: localStorage.getItem('token'),
+    }
+  }),
+  typeDefs,
+  resolvers,
 })
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  }
+})
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`
+
+const IsLoggedIn = () => {
+  const { data } = useQuery(IS_LOGGED_IN)
+  return data.isLoggedIn ? <Pages /> : <Login />
+}
 
 
 ReactDOM.render(
